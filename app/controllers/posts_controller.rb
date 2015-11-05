3485979f4fcf941069516912d5b63766994191a7
Jamaleh <Jamaleh@hotmail.com>
2015-11-05 21:02:04 +1100

    add pagination to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :destroy, :edit, :update]
   def index
-    @posts = Post.all.order("created_at desc")
+    @posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 10)
   end
 
   def edit
