84bf45d6e6a5a854cb6a9891601772651b10a2eb
Jamaleh <Jamaleh@hotmail.com>
2015-11-05 17:45:45 +1100

    re apply before action to posts model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,13 @@
 class PostsController < ApplicationController
-  # before_action :find_post, only: [:show, :create, :destroy, :edit, :update]
-
+  before_action :find_post, only: [:show, :destroy, :edit, :update]
   def index
     @posts = Post.all
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update post_params
       redirect_to @post, notice: "Right on!  Saved!"
     else
@@ -32,21 +29,20 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
+
 
   private
   def post_params
     params.require(:post).permit(:title, :content)
   end
 
-  # def find_post
-  #   @post = Post.find(params[:id])
-  # end
+  def find_post
+    @post = Post.find(params[:id])
+  end
 end
