851ebe31ec21ca5c7d6d3f9ceda8ffe2944e56d8
Jamaleh <Jamaleh@hotmail.com>
2015-11-05 17:07:13 +1100

    update stylesheet to scss and blog show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,17 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show, :create, :destroy, :edit, :update]
-  def index
-  end
+  # before_action :find_post, only: [:show, :create, :destroy, :edit, :update]
 
-  def show
+  def index
+    @posts = Post.all
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
-    if @post.update(post_params)
+    @post = Post.find(params[:id])
+    if @post.update post_params
       redirect_to @post, notice: "Right on!  Saved!"
     else
       render 'edit', notice: "DANG!  Try again!"
@@ -22,7 +23,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = Post.new post_params
     if @post.save
       redirect_to @post, notice: "Yeah!  That was a great BLOG that you posted!"
     else
@@ -30,7 +31,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def destroy
+    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -40,7 +46,7 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :content)
   end
 
-  def find_post
-    @post = Post.find(params[:id])
-  end
+  # def find_post
+  #   @post = Post.find(params[:id])
+  # end
 end
